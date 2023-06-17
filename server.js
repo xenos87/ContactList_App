0ca9ac71af38ae92232f443eb18c9138d4202261
Elom <xenosbazz87@gmail.com>
2023-06-17 16:23:11 +0200

    path resolved

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 // Init middleware
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/dist/index.html')));
 
 // Defines Routes
 app.use('/api/users', require('./routes/users'));
