7ca91858d414704d51782d12605cb8b3dab916ce
Elom <xenosbazz87@gmail.com>
2023-06-17 17:04:05 +0200

    server resolved

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
+const path = require('path')
 
 
 const app = express();
@@ -9,16 +9,22 @@ const app = express();
 connectDB();
 
 // Init middleware
-app.use(express.static(__dirname + '/dist/build'));
+app.use(express.json({ extended: false}));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/dist/index.html'));
+
 
 // Defines Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 
 const PORT = process.env.PORT || 5000;
